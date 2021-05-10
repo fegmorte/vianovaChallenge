e828ffd8343678250ae80625f99008540b3fdfc7
fegmorte <frederic.egmorte@tessi.fr>
2021-05-10 23:45:36 +0200

    update gitignore

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -6,8 +6,3 @@ dir_name=lambda_dist_pkg/
 mkdir $dir_name
 
 cp -r $path_cwd/lambda_function/ $path_cwd/$dir_name
-
-# Removing virtual environment folder...
-echo "Removing virtual environment folder..."
-rm -rf $path_cwd/env_$function_name
-echo "Finished script execution!"
